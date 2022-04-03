e7dff29c870d73af0d3d75f12f68587f2ba9d436
RobertoHiro <mrhiroshiroberto@yahoo.com.br>
2022-04-03 13:48:50 -0300

    implementando carousel multi itens

--- desafio/js/main_function.js
+++ desafio/js/main_function.js
@@ -6,3 +6,35 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+var multipleCardCarousel = document.querySelector(
+  "#tiposDeLimpezaCarousel"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#tiposDeLimpezaCarousel .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#tiposDeLimpezaCarousel .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#tiposDeLimpezaCarousel .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#tiposDeLimpezaCarousel .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
