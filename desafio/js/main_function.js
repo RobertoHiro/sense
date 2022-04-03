3285e85d6ed5eb251eefa057f62dd5d8374df5fb
RobertoHiro <mrhiroshiroberto@yahoo.com.br>
2022-04-03 11:27:26 -0300

    melhoramento no responsivo do saibaMaisLimpeza

--- desafio/js/main_function.js
+++ desafio/js/main_function.js
@@ -6,16 +6,3 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-/*
-$(window).resize(function(){
-   console.log('resize called');
-   var width = $(window).width();
-   if(width >= 768){
-       $('#nossosServicos').removeClass('justify-content-between').addClass('justify-content-start');
-   }
-   else{
-       $('#nossosServicos').removeClass('justify-content-start').addClass('justify-content-between');
-   }
-})
-.resize();
-*/
