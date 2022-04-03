c65cac205daa6403a0b83df9495cd27f421d2ea0
RobertoHiro <mrhiroshiroberto@yahoo.com.br>
2022-04-02 22:48:30 -0300

    melhoria no carrosel

--- desafio/js/main_function.js
+++ desafio/js/main_function.js
@@ -6,3 +6,16 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+/*
+$(window).resize(function(){
+   console.log('resize called');
+   var width = $(window).width();
+   if(width >= 768){
+       $('#nossosServicos').removeClass('justify-content-between').addClass('justify-content-start');
+   }
+   else{
+       $('#nossosServicos').removeClass('justify-content-start').addClass('justify-content-between');
+   }
+})
+.resize();
+*/
